9377d8cf77ccb246acc2bfec9bf023df6e46ba97
CSchemmy <CSchemmy@web.de>
2018-02-22 22:22:48 +0100

    do only one time replace APACHEDS_DOMAIN/APACHEDS_TOP_DOMAIN

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -x
+
+#set -x
+
 # Environment variables:
 # APACHEDS_VERSION
 # APACHEDS_INSTANCE
@@ -7,6 +9,8 @@ set -x
 # APACHEDS_DATA
 # APACHEDS_USER
 # APACHEDS_GROUP
+# APACHEDS_DOMAIN
+# APACHEDS_TOP_DOMAIN
 
 APACHEDS_INSTANCE_DIRECTORY=${APACHEDS_DATA}/${APACHEDS_INSTANCE}
 
@@ -18,18 +22,21 @@ function replaceTopDomain () {
   sed -i "s#\\\${APACHEDS_TOP_DOMAIN}#${APACHEDS_TOP_DOMAIN}#g" $1
 }
 
+function replaceAtFile () {
+  replaceDomain $1
+  replaceTopDomain $1
+}
+
 # When a fresh data folder is detected then bootstrap the instance configuration.
 if [ ! -d ${APACHEDS_INSTANCE_DIRECTORY} ]; then
     mkdir ${APACHEDS_INSTANCE_DIRECTORY}
     cp -rv ${APACHEDS_BOOTSTRAP}/* ${APACHEDS_INSTANCE_DIRECTORY}
     chown -v -R ${APACHEDS_USER}:${APACHEDS_GROUP} ${APACHEDS_INSTANCE_DIRECTORY}
+    replaceAtFile ${APACHEDS_INSTANCE_DIRECTORY}/conf/config.ldif
+    replaceAtFile ${APACHEDS_INSTANCE_DIRECTORY}/ome.ldif
+    replaceAtFile contextentry.template
+    sed -i "s#\\\${CONTEXTENTRY}#`base64 contextentry.template | tr --delete '\n'`#" ${APACHEDS_INSTANCE_DIRECTORY}/conf/config.ldif
 fi
-
-replaceDomain ${APACHEDS_INSTANCE_DIRECTORY}/conf/config.ldif
-replaceTopDomain ${APACHEDS_INSTANCE_DIRECTORY}/conf/config.ldif
-replaceDomain contextentry.template
-replaceTopDomain contextentry.template
-sed -i "s#\\\${CONTEXTENTRY}#`base64 contextentry.template | tr --delete '\n'`#" ${APACHEDS_INSTANCE_DIRECTORY}/conf/config.ldif
 
 # Execute the server in console mode and not as a daemon.
 /opt/apacheds-${APACHEDS_VERSION}/bin/apacheds console ${APACHEDS_INSTANCE}
